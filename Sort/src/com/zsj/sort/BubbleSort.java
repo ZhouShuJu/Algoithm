a6cbb341fa8e1ad194708a1e3b3f252f6e8203a5
brown <a903233140@126.com>
2017-11-19 22:51:18 +0800

    解决冒泡排序下标越界

--- Sort/src/com/zsj/sort/BubbleSort.java
+++ Sort/src/com/zsj/sort/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort implements SortDao {
 
 	@Override
 	public void sort(int[] arr) {
-		for (int i = arr.length; i > 0; i--) {
+		for (int i = arr.length - 1; i > 0; i--) {
 			for (int j = 0; j < i; j++) {
 				if (arr[j] > arr[j + 1]) {
 					SortUtil.swap(arr, j, j + 1);
