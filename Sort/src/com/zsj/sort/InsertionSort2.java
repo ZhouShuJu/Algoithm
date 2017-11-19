08ef0ed5f0344732efbe7343a487fd3ab82398ff
brown <a903233140@126.com>
2017-11-19 14:48:00 +0800

    实现了改进的insertion排序法
    在test类中进行了测试，但是跟预想的有出入，再进行测试改进

--- Sort/src/com/zsj/sort/InsertionSort2.java
+++ Sort/src/com/zsj/sort/InsertionSort2.java
@@ -2,11 +2,20 @@ package com.zsj.sort;
 
 import com.zsj.sort.SortDao;
 
-
+/**
+ * 
+ * @author ZhouShuJu
+ * @date   2017年11月19日 14:01:36
+ */
 public class InsertionSort2 implements SortDao{
 
-    public void sort(int [] arr) {
-        
+    public void sort(int [] arr) {    
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            for (int j = i; j > 0 && arr[j - 1] > temp; j--) {
+                arr[j] = arr[j - 1];
+            }
+        }
     }
 
 }
--- Sort/src/com/zsj/test/SortTest.java
+++ Sort/src/com/zsj/test/SortTest.java
@@ -3,6 +3,7 @@ package com.zsj.test;
 import java.util.Arrays;
 
 import com.zsj.sort.InsertionSort;
+import com.zsj.sort.InsertionSort2;
 import com.zsj.sort.SelectionSort;
 import com.zsj.sort.SortDao;
 import com.zsj.util.SortUtil;
@@ -17,21 +18,28 @@ public class SortTest {
 	public static void main(String[] args) {
 		
 		//生成数组
-		int n = 100000;
+		int n = 10000;
 		int [] arr = SortUtil.generateRandomArray(n, 0, n);
 		int [] arrCopy = Arrays.copyOf(arr, n);
+		int [] arrCopy2 = Arrays.copyOf(arr, n);
 		int [] almostSortedArr = SortUtil.generateAlmostSortedArr(n, 10);
 		int [] almostSortedArrCopy = Arrays.copyOf(almostSortedArr, n);
-		
+		int [] almostSortedArrCopy2 = Arrays.copyOf(almostSortedArr, n);
+
 	    //创建排序类
 		SortDao selectionSort = new SelectionSort();
 		SortDao insertionSort = new InsertionSort();
-		
+		SortDao insertionSort2 = new InsertionSort2();
+
+
 		SortUtil.sortTest("SelectionSort", selectionSort, arr);
 		SortUtil.sortTest("SelectionSort-a", selectionSort, almostSortedArr);
 		SortUtil.sortTest("InsertionSort", insertionSort, arrCopy);
-		SortUtil.sortTest("InsertionSrot-a", insertionSort, almostSortedArrCopy);
-		
+		SortUtil.sortTest("InsertionSort-a", insertionSort, almostSortedArrCopy);
+		SortUtil.sortTest("InsertionSort2", insertionSort2, arrCopy2);
+		SortUtil.sortTest("InsertionSort2-a", insertionSort2, almostSortedArrCopy2);
+
+
 		/*
 		//排序
 		selectionSort.sort(arr);
