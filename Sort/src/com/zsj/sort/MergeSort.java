ed12f13b8aa77cd052df503c3cd137cfdf3fe71f
brown <a903233140@126.com>
2017-11-19 21:51:21 +0800

    实现了merge排序，还没有测试，不知是否正确

---
+++ Sort/src/com/zsj/sort/MergeSort.java
@@ -0,0 +1,43 @@
+package com.zsj.sort;
+/**
+ * 归并排序法
+ * @Author ZhouShuJu
+ * @date   2017年11月19日下午9:14:29
+ */
+public class MergeSort implements SortDao {
+
+	@Override
+	public void sort(int[] arr) {
+		mergeSort(arr, 0, arr.length-1); 
+	}
+
+	private void mergeSort(int[] arr, int l, int r) {
+		if (l >= r) {
+			return;
+		}
+		int mid = (l + r) / 2;
+		mergeSort(arr, l, mid);
+		mergeSort(arr, mid + 1, r);
+		merge(arr, l, r);
+	}
+
+	private void merge(int[] arr, int l, int r) {
+		int [] temp = arr;
+		int mid = (l + r) / 2;
+		int i = l, j = mid + 1;
+		for (int k = l; k <= r; k++) {
+			if (l > mid) {
+				arr[k] = temp[j];
+			} else if (j > r) {
+				arr[k] = temp[l];
+			} else if (temp[l] < temp[j]) {
+				arr[k] = temp[l];
+				l++;
+			} else {
+				arr[k] = temp[j];
+				j++;
+			}
+		}
+	}
+
+}
